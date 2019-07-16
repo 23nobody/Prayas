9f8d88b46c0dd1168027e6324677b1ba098adeb0
nobody23 <iit2018073@iiita.ac.in>
2019-07-17 04:59:12 +0530

    spinner setup

--- app/src/main/java/android/example/com/prayas/EditorActivity.java
+++ app/src/main/java/android/example/com/prayas/EditorActivity.java
@@ -1,6 +1,73 @@
 package android.example.com.prayas;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-public class EditorActivity extends Activity {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditorActivity extends AppCompatActivity {
+    private EditText mNameEdit;
+    private EditText mRollEdit;
+    private EditText mBatchEdit;
+    private Spinner mGenderSpinner;
+    public final int GENDER_UNKNOWN=0;
+    public final int GENDER_MALE=1;
+    public final int GENDER_FEMALE=2;
+    private int mGender = GENDER_UNKNOWN;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+        setTitle("Edit Member");
+        mNameEdit = (EditText) findViewById(R.id.edit_name);
+        mRollEdit = (EditText) findViewById(R.id.edit_roll);
+        mBatchEdit = (EditText) findViewById(R.id.edit_batch);
+        mGenderSpinner = (Spinner) findViewById(R.id.spinner_gender);
+        setupSpinner();
+
+    }
+    private void setupSpinner() {
+        // Create adapter for spinner. The list options are from the String array it will use
+        // the spinner will use the default layout
+        ArrayAdapter genderSpinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.array_gender_options, android.R.layout.simple_spinner_item);
+
+        // Specify dropdown layout style - simple list view with 1 item per line
+        genderSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+
+        // Apply the adapter to the spinner
+        mGenderSpinner.setAdapter(genderSpinnerAdapter);
+
+        // Set the integer mSelected to the constant values
+        mGenderSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selection = (String) parent.getItemAtPosition(position);
+                if (!TextUtils.isEmpty(selection)) {
+                    if (selection.equals(getString(R.string.gender_male))) {
+                        mGender = GENDER_MALE;
+                    } else if (selection.equals(getString(R.string.gender_female))) {
+                        mGender = GENDER_FEMALE;
+                    } else {
+                        mGender = GENDER_UNKNOWN;
+                    }
+                }
+            }
+
+            // Because AdapterView is an abstract class, onNothingSelected must be defined
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                mGender = GENDER_UNKNOWN;
+            }
+        });
+    }
+
 }
