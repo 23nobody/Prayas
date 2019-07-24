64e8942e497064308bb68e68074fe0114da3f1d6
BinduVerma123 <bindu100958@gmail.com>
2019-07-25 01:04:31 +0530

    thre

--- app/src/main/java/android/example/com/prayas/Set1.java
+++ app/src/main/java/android/example/com/prayas/Set1.java
@@ -20,16 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public  class Set1 extends Fragment {
-
-    private StudentAdapter mStudentAdapter;
-    private ChildEventListener mChildEventListener;
-    ArrayList<String> arrayList = new ArrayList<>();
-    private ListView mListView;
-    String name[] = {"bindu", "mummy", "dramil"};
-    String roll[] = {"201", "205", "456"};
-    private DatabaseReference mStudentDatabaseReference;
-
-    //mListView=findViewById(R.id.list);
+    
     public Set1() {
     }
 
@@ -37,45 +28,12 @@ public  class Set1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.content_scrolling, container, false);
-        mListView = rootView.findViewById(R.id.list);
 
         return rootView;
         //set_dapter();
 
     }
-        public void attchDatabaseListener() {
-            if(mChildEventListener == null) {
-                mChildEventListener = new ChildEventListener() {
-                    @Override
-                    public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                        Student friendlyMessage = dataSnapshot.getValue(Student.class);
-                        mStudentAdapter.add(friendlyMessage);
 
-                    }
-
-                    @Override
-                    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-                    }
-
-                    @Override
-                    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-                    }
-
-                    @Override
-                    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                };
-                mStudentDatabaseReference.addChildEventListener(mChildEventListener);
-            }
-        }
 
 
 }
