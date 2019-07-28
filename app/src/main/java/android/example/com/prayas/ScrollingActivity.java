e8eb8a531623a7c09a6cf1a88d4b3b5879c8170d
nobody23 <iit2018073@iiita.ac.in>
2019-07-29 01:42:46 +0530

    implemented date in scrolling_activity

--- app/src/main/java/android/example/com/prayas/ScrollingActivity.java
+++ app/src/main/java/android/example/com/prayas/ScrollingActivity.java
@@ -1,6 +1,7 @@
 package android.example.com.prayas;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -15,10 +16,15 @@ import androidx.viewpager.widget.ViewPager;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class ScrollingActivity extends AppCompatActivity {
+    EditText date;
+    DatePickerDialog datePickerDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +49,30 @@ public class ScrollingActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        date = (EditText) findViewById(R.id.date);
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calender class's instance and get current date , month and year from calender
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(ScrollingActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                date.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();            }
+        });
 
 
 
@@ -66,6 +96,7 @@ public class ScrollingActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         switch (id){
             case R.id.submit_button:
+                String ddd=date.getText().toString().trim();
                 return true;
             case R.id.action_settings:
                 return true;
